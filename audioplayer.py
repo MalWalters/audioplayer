4e6ad1cca60ab3e3ee33f61aca18b3b6d5cfff31
Malcolm Walters <MalWalters@users.noreply.github.com>
2017-12-18 17:00:18 +1100

    Create audioplayer.py
    Reads json data files

---
+++ audioplayer.py
@@ -0,0 +1,40 @@
+from classes.book import Book
+import time, datetime
+import json
+#import RPI.GPIO as GPIO
+
+def start():
+    print(currentBookID[0]['currentBookID'])
+    currentBookID[0]['currentBookID'] = currentBookID[0]['currentBookID'] + " again|"
+    return
+
+def stop():
+    print("stop")
+    return
+
+def switch():
+    
+    return
+
+#load libray, rfid reference table and current book ID from json
+with open('json/rfid.json') as data_file:
+    rfid = json.load(data_file)
+
+with open('json/library.json') as data_file:
+    library = json.load(data_file)
+
+with open('json/currentbook.json') as data_file:
+    currentBookID = json.load(data_file)
+
+while True:
+    n = input ("Start, Stop, Switch: ")
+    if n == "break":
+        break
+    elif n == "switch":
+        switch()
+    elif n == "start":
+        start()
+    elif n == "stop":
+        stop()
+
+

5bb3bf617c0dce67593704f8a3577b3b2f0658f7
Malcolm Walters <MalWalters@users.noreply.github.com>
2017-12-18 17:01:20 +1100

    Create books.py
    Initial class description for books

---
+++ classes/books.py
@@ -0,0 +1,12 @@
+#This is the class file for audio player
+
+class Book:
+	""" Base Book class"""
+	def __init__(self, id, title, url, playtime):
+		self.id = id
+		self.title = title
+		self.url = url
+		self.playtime = playtime
+
+	def addPlaytime(currentPlaytime):
+		self.playTime = currentPlayTime
